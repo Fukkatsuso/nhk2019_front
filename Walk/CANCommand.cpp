f4153679ab190d3e57baa8dd75ce77919d5f75a3
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2019-02-18 21:12:48 +0900

    Edit CANCommand-receive

--- Walk/CANCommand.cpp
+++ Walk/CANCommand.cpp
@@ -8,7 +8,54 @@
 #include "CANCommand.h"
 
 
+short CANformats[DataType_end][FormatType_end] =
+{		//ID,	Len_integer,	Len_fraction
+		{Period,	1,	4},	//Period
+		{Duty,		1,	4},	//Duty
+		{Speed,		3,	4},	//Speed
+		{Direction,	2,	4},	//Direction
+		{Time,		2,	4},	//Time
+		{Area,		2,	0}	//Area
+};
+
+
 CANCommand::CANCommand(CAN *can)
 {
 	this->can = can;
+}
+
+
+/*
+ * example:
+ * CANCommand cancmd(&can);
+ * void can_receive(){
+ * 		if(can.read(rcvMsg)){
+ * 			if((rcvMsg.id&0xff0)==ID_sndAll){
+ * 				cancmd.recerive(rcvMsg.id, rcvMsg.data);
+ * 			}
+ * 		}
+ * }
+ */
+void CANCommand::receive(unsigned int id, unsigned char data[])
+{
+	id = (id)&(0x00f);//下一桁のみ取り出す
+	if(0<=id && id<DataType_end)
+		rcvData[id] = decode_from_array(data, CANformats[id][Len_integer], CANformats[id][Len_fraction]);
+}
+
+
+float CANCommand::get(enum DataType type){
+	return rcvData[type];
+}
+
+
+float CANCommand::decode_from_array(unsigned char array[], int len_i, int len_f)
+{
+	float value=0;
+	for(int i=1; i<=len_i; i++)
+		value += array[i]*(int)pow(10, len_i-i);
+	for(int i=len_i+1; i<=len_i+len_f; i++)
+		value += (float)array[i]*(float)pow(0.1, i-len_i);
+	if(array[0])value *= -1;
+	return value;
 }
--- Walk/CANCommand.h
+++ Walk/CANCommand.h
@@ -4,11 +4,35 @@
  *  Created on: 2019/02/16
  *      Author: mutsuro
  */
+//新たに受信する変数を追加するときには
+//DataType, FormatType の末尾(_endの前)に変数名を追加すること
+//CANformats[][]の末尾に要素を追加すること
+
 
 #ifndef WALK_CANCOMMAND_H_
 #define WALK_CANCOMMAND_H_
 
 #include "mbed.h"
+
+
+enum DataType{
+	Period=0,
+	Duty,
+	Speed,
+	Direction,
+	Time,
+	Area,
+	DataType_end//<=0x00f=15 に制限（仕様上）
+};
+
+enum FormatType{
+	ID=0,
+	Len_integer,
+	Len_fraction,
+	FormatType_end
+};
+
+extern short CANformats[DataType_end][FormatType_end];
 
 
 /*
@@ -18,10 +42,19 @@ class CANCommand
 {
 public:
 	CANCommand(CAN *can);
-	int get_area();
+	void receive(unsigned int id, unsigned char data[]);
+	//void send(int ID, int , float data);後回し
+
+	float get(enum DataType type);
+
+protected:
+	float decode_from_array(unsigned char array[], int len_i, int len_f);
+	void store_in_data(float f, int len_i, int len_f);
+	void copy_data(CANMessage *msg, int len);
 
 private:
 	CAN *can;
+	float rcvData[DataType_end];
 };
 
 
