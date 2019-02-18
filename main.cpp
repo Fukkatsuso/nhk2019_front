57eddc66072da37f23d1d81f05f782aae5f0cb98
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2019-02-18 11:50:01 +0900

    Add PID-setting

--- main.cpp
+++ main.cpp
@@ -11,8 +11,16 @@ SingleLeg FRr(Rear, Right, -BASE_X, 0);
 SingleLeg FLf(Front, Left, BASE_X, 0);
 SingleLeg FLr(Rear, Left, -BASE_X, 0);
 
+void read_PIDgain(float gain[], const char *fileName);
 
+
+/******************
+ * 		main	  *
+ ******************/
 int main(){
+	float gain[3] = {0, 0, 0};
+	can.frequency(1000000);
+	//can.attach(, CAN::RxIrq);
 	wait_ms(300); //全ての基板の電源が入るまで待つ
 	pc.baud(921600);
 
@@ -22,9 +30,35 @@ int main(){
 	FRr.unitize(&motor_FRr, &enc_FRr, &sw_FRr);
 	FLf.unitize(&motor_FLf, &enc_FLf, &sw_FLf);
 	FLr.unitize(&motor_FLr, &enc_FLr, &sw_FLr);
-
+	FRf.lengths(LEG_UPPER, LEG_FORE);
+	FRr.lengths(LEG_UPPER, LEG_FORE);
+	FLf.lengths(LEG_UPPER, LEG_FORE);
+	FLr.lengths(LEG_UPPER, LEG_FORE);
+	read_PIDgain(gain, "/local/PID_FRf.txt");
+	FRf.set_PID(gain[0], gain[1], gain[2]);
+	read_PIDgain(gain, "/local/PID_FRr.txt");
+	FRr.set_PID(gain[0], gain[1], gain[2]);
+	read_PIDgain(gain, "/local/PID_FLf.txt");
+	FLf.set_PID(gain[0], gain[1], gain[2]);
+	read_PIDgain(gain, "/local/PID_FLr.txt");
+	FLr.set_PID(gain[0], gain[1], gain[2]);
 
 	while(1){
 		AdjustCycle(5000);
 	}
+}
+
+
+void read_PIDgain(float gain[], const char *fileName){
+	FILE *fp = fopen(fileName, "r");
+	pc.printf("\r\n");
+	if(!fp){
+		pc.printf("file open error!!\r\n");
+		return;
+	}
+	for(int i=0; i<3; i++){
+		fscanf(fp, "%f", &gain[i]);
+		pc.printf("%f  ", gain[i]);
+	}
+	fclose(fp);
 }
